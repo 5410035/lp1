db51691fb98e72e38cea551233fc3cb663c38543
5410035 <5410035sryuji331@gmail.com>
2024-11-25 16:45:24 +0900

    パーフェクトピクセルで確認、修正完了

--- assets/script/main.js
+++ assets/script/main.js
@@ -7,6 +7,10 @@ $(window).on('resize', function () {
 	hamburger.removeClass('hamburger-menu_active');
 });
 
+$('.header-nav__item-link').on('click', function () {
+	hamburger.toggleClass('hamburger-menu_active');
+});
+
 // タブ
 $('#smarttab').smartTab({
     enableUrlHash: false
