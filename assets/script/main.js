a6ef7cf77c83c24681fc74fa673abcf815477739
5410035 <5410035sryuji331@gmail.com>
2024-12-21 04:04:32 +0900

    241221_指摘事項修正中

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,14 +1,14 @@
 // ハンバーガーメニュー
 var hamburger = $('.hamburger-menu');
 $('.hamburger__button').on('click', function () {
-	hamburger.toggleClass('hamburger-menu_active');
+    hamburger.toggleClass('hamburger-menu_active');
 });
 $(window).on('resize', function () {
-	hamburger.removeClass('hamburger-menu_active');
+    hamburger.removeClass('hamburger-menu_active');
 });
 
 $('.header-nav__item-link').on('click', function () {
-	hamburger.toggleClass('hamburger-menu_active');
+    hamburger.toggleClass('hamburger-menu_active');
 });
 
 // タブ
